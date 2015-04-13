b4d69358e244b52f546d9013fca94657514be2d6
ichyo <syo1990@gmail.com>
2015-04-14 03:38:41 +0900

    Add usage

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AOJ-Solution-Crawler
 Correcting solutions of the problems in Aizu online judge
+
+## usage
+```
+from crawler import AOJSolutionCrawler
+
+def handle(problem, user, language, solution):
+    print("{} solved {} by {}".format(user, problem, language))
+    print("solution:\n{}\n".format(solution))
+
+crawler = AOJSolutionCrawler(handle)
+crawler.crawle()
+```
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from crawler import AOJSolutionCrawler
 
 def main():
-    def handle(pid, uid, lang, sol):
-        print("{} {} {} {}".format(pid, uid, lang, sol))
-    a = AOJSolutionCrawler(handle)
-    a.crawle()
+    def handle(problem, user, language, solution):
+        print("{} solved {} by {}".format(user, problem, language))
+        print("solution:\n{}\n".format(solution))
+
+    crawler = AOJSolutionCrawler(handle)
+    crawler.crawle()
 
 if __name__ == '__main__':
     main()
